a489b36215c932538552513f4b838ec7f81f49ba
lucasgaleano <l.a.galeano2@gmail.com>
2021-10-26 00:37:16 -0300

    add progress bar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cis-to-sheet
+
+## Install
+First install fitz then pymupdf.
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import fitz
 import argparse
 import re
 import time
+from alive_progress import alive_bar
 
 parser = argparse.ArgumentParser()
 parser.add_argument('cisDocument', type=str)
@@ -10,17 +11,18 @@ args = parser.parse_args()
 pdf = fitz.open(args.cisDocument)
 
 
-end = '(Remedation:|P a g e| Audit:| Rationale:| Description:| Profile Applicability:|CIS Controls:|Impact:|References:|Notes:|$)'
-fields = dict()
+end = '(Remediation:|P a g e| Audit:| Rationale:| Description:| Profile Applicability:|CIS Controls:|Impact:|References:|Notes:|$)'
+end2 = '(Remediation:|P a g e| Audit:| Rationale:| Description:| Profile Applicability:|CIS Controls:|Impact:|References:|Notes:)'
 
-fields['ID'] = 'P a g e\s*([\d.]*)(.*?)' + end 
-fields['title'] = 'P a g e\s*[\d.]*(.*?)\s*' + end
-fields['profile'] = 'Profile Applicability:(.*?)' + end 
-fields['description'] = 'Description: (.*?)' + end 
-fields['rationale'] = 'Rationale: (.*?) ' + end 
-fields['impact'] = 'Impact: (.*?) ' + end 
-fields['audit'] = 'Audit: (.*?)' + end 
-fields['remediation'] = 'Remediation: (.*?)' + end 
+fields = dict()
+fields['ID'] = r'P a g e\s*([\d.]+)(.*?)' + end2 
+fields['title'] = r'P a g e\s*[\d.]+(.*?)\s*' + end2
+fields['profile'] = r'Profile Applicability:(.*?)' + end 
+fields['description'] = r'Description: (.*?)' + end 
+fields['rationale'] = r'Rationale: (.*?) ' + end 
+fields['impact'] = r'Impact: (.*?) ' + end 
+fields['audit'] = r'Audit: (.*?)' + end 
+fields['remediation'] = r'Remediation: (.*?)' + end 
 
 
 
@@ -31,11 +33,25 @@ def match(regex):
         return matchRegex.group(1)
         #print(title, matchRegex.group(1))
 
+def quotes_text(text: str) -> str:
+    print('"', end='')
+    print(text.replace('"','""'),end='')
+    print('"', end='')
 
 
-for page in pdf:
-    print('###############################################\n\n\n')
-    print(page.get_text().replace('\t',' '))
-    for nameField, field in fields.items():
-        text = match(field)
-        print(nameField,'####',text)
+with alive_bar(pdf.page_count ,title='Number of Pages', length=30) as bar:
+    for page in pdf:   
+        bar()
+        
+        # print('###############################################\n\n\n') #DEBUG
+        # print(page.get_text().replace('\t',' ')) #DEBUG
+        for nameField, field in fields.items():
+            text = match(field)
+            # if text is not None:
+                # quotes_text(text)
+                # if "Remediation" == nameField:
+                    # print()
+                # else:
+                    # print(',', end='')
+            
+            # print(nameField,'####',text) #DEBUG
